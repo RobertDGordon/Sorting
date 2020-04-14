f00474914d269583c78a4cf6516a42bf13af2d7e
Robert Gordon <Komeuppance@gmail.com>
2020-04-14 12:52:02 -0700

    Lecture examples

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -41,11 +41,36 @@ def bubble_sort( arr ):
 bubble_sort(testArr)
 print(f'\n{testArr}')
 
+def bubble_sort2( arr ):
+    # Make a flag to show if swaps have occured
+    swaps_occured = True
+    # Run until you get through a loop without any swaps
+    while swaps_occured:
+        swaps_occured = False
+        # For each element in the array...
+        for i in range(len(arr) - 1):
+            print(arr)
+            # Check it's neighbor to the right...
+            if arr[i] > arr[i+1]:
+                # If neighbor is smaller, swap and make Flag true
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swaps_occured = True
+
+# def bubble_sort2(arr):
+#     swaps_occured = True
+#     while swaps_occured:
+#         swaps_occured: False
+#         for i in range(len(arr) - 1):
+#             if arr[i] > arr[i+1]:
+#                 arr[i], arr[i+1] = arr[i+1], arr[i]
+#                 swaps_occured = True
+
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
     return arr
-
+#Time complexity: O(n^2)
+#Space complexity: O(1)
 # def insertion_sort(arr):
 #     for i in range(1, len(arr)):
 #         temp = arr[1]
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -31,3 +31,49 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+
+#########################################
+def factorial(n):
+    total = 1
+    for i in range(1, n+1):
+        total *= i
+    return total
+
+def factorial(n):
+    if n <= 1: #base case
+        return 1 
+    return n * factorial(n-1)
+
+#######################################
+def quicksort(arr):
+    if len(arr) <=1:
+        return arr
+    pivot = arr[0]
+    smaller = []
+    larger = []
+    for i in range(1, len(arr)):
+        if arr[i] <= pivot:
+            smaller.append(arr[i])
+        else:
+            larger.append(arr[i])
+    # smaller = quicksort(smaller)
+    # larger = quicksort(larger)
+    return quicksort(smaller) + [pivot] + quicksort(larger)
+
+# MERGESORT
+# [2, 0, 1, 3, 6, 9, 8, 5, 7, 4]
+# ​
+# # (base case) If the array is empty or length 1, return
+# ​
+# # Split the arrays into half
+# arr1 = [2, 0, 1, 3, 6]
+# arr2 = [9, 8, 5, 7, 4]
+# ​
+# # Sort each half
+# arr1 = [0,1,2,3,6]
+# arr2 = [4,5,7,8,9]
+# ​
+# # Merge them back together
+# # Compare the first values of each array, add smaller of the 2 to result
+# merged = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
